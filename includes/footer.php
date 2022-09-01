9086e31c952a029b59eef0a9853ae2345e73df7e
ammielpeters221 <100462995+ammielpeters221@users.noreply.github.com>
2022-09-01 23:06:13 +0530

    Website

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Jorht">PG in Jorhat</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Guwahati">PG in Guwahati</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Dibrugarh">PG in Dibrugarh</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Nagaon">PG in Nagaon</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
